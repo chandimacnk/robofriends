65695142f89ef332ff987889e585dee815fd04b3
chandimacnk <chandimcnk@gmail.com>
2020-12-08 16:48:04 +0530

    Fixed a small UI issue

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,22 +2,35 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
+    // if(true){
+    //     throw new Error('NOOOOOOOO!');
+    // }
+    // const cardArray = robots.map((user, i) => {
+    //     return (
+    //         <Card 
+    //             key={i} 
+    //             id={robots[i].id} 
+    //             name={robots[i].name} 
+    //             email={robots[i].email} 
+    //         />
+    //     )
+    // })
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                            key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    )
+                })
+            }
+        </div>
+    );
 }
 
 export default CardList;
